94449efe0f31cbeb5e6923147af83e9328d60e37
Victor Dallé <dallevictor@gmail.com>
2024-03-13 16:53:57 +0100

    modif cmake pour ne pas mettre required openmp

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@
 int main(int argc, char **argv)
 {
 
-    const int width = 2000;
-    const int height = 2000;
+    const int width = 800;
+    const int height = 800;
 
     TGAImage image(width, height, TGAImage::RGB);
 
-    std::string filename = "../obj/african_head.obj";
+    std::string filename = "../obj/african_head/african_head.obj";
 
     if (argc >= 2)
     {
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
 
     Model model(filename, width, height);
 
-    // model.drawTriangles(image, white);
+    model.drawTriangles(image, white);
     model.drawVertices(image, green);
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
