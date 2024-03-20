7c7fc616098d6081b7a003811fcc0333f319407c
Victor Dallé <dallevictor@gmail.com>
2024-03-20 12:06:43 +0100

    ajout back-face culling + fill triangles

--- src/model.hpp
+++ src/model.hpp
@@ -32,14 +32,27 @@ struct Model
                 // Denormalize the point to match the screen size
                 p.x = ((p.x + 1.) * 0.5 * width);
                 p.y = ((p.y + 1.) * 0.5 * height);
+                p.z *= 800;
+
+                float r = (p.x + 1.) * 0.5 * 255;
+                float g = (p.y + 1.) * 0.5 * 255;
+                float b = (p.z + 1.) * 0.5 * 255;
+                p.color = TGAColor(r, g, b, 255);
+
                 vertices.push_back(p);
             }
 
             if (line[0] == 'f' && line[1] == ' ')
             {
-                Triangle shape;
-                Triangle texture;
-                Triangle normal;
+                struct tmp
+                {
+                    int p1;
+                    int p2;
+                    int p3;
+                };
+                tmp shape;
+                tmp texture;
+                tmp normal;
                 sscanf(line.c_str(), "f %d/%d/%d %d/%d/%d %d/%d/%d", &shape.p1, &texture.p1, &normal.p1, &shape.p2, &texture.p2, &normal.p2, &shape.p3, &texture.p3, &normal.p3);
 
                 // Decrement the indices to match the 0-based array
@@ -55,16 +68,16 @@ struct Model
                 normal.p2--;
                 normal.p3--;
 
-                triangles.push_back(shape);
-                textures.push_back(texture);
-                normals.push_back(normal);
+                triangles.push_back(Triangle(vertices[shape.p1], vertices[shape.p2], vertices[shape.p3]));
+                textures.push_back(Triangle(vertices[texture.p1], vertices[texture.p2], vertices[texture.p3]));
+                normals.push_back(Triangle(vertices[normal.p1], vertices[normal.p2], vertices[normal.p3]));
             }
         }
     }
 
     void drawVertices(TGAImage &image, TGAColor color)
     {
-        int i = 0;
+#pragma omp parallel for
         for (Point p : vertices)
         {
             image.set(p.x, p.y, color);
@@ -73,11 +86,39 @@ struct Model
 
     void drawTriangles(TGAImage &image, TGAColor color)
     {
+#pragma omp parallel for
         for (Triangle t : triangles)
         {
-            draw_line(vertices[t.p1], vertices[t.p2], image, color);
-            draw_line(vertices[t.p1], vertices[t.p3], image, color);
-            draw_line(vertices[t.p2], vertices[t.p3], image, color);
+            draw_line(t.p1, t.p2, image, color);
+            draw_line(t.p1, t.p3, image, color);
+            draw_line(t.p2, t.p3, image, color);
+        }
+    }
+
+    void fillTriangles(TGAImage &image)
+    {
+#pragma omp parallel for
+        for (Triangle t : triangles)
+        {
+            fillTriangle(t, image, randomColor());
+        }
+    }
+
+    void fillTrianglesLerp(TGAImage &image)
+    {
+#pragma omp parallel for
+        for (Triangle t : triangles)
+        {
+            fillTriangleLerp(t, image);
+        }
+    }
+
+    void fillTrianglesWithBackFaceCulling(TGAImage &image)
+    {
+#pragma omp parallel for
+        for (int i = 0; i < triangles.size(); i++)
+        {
+            fillTriangleWithBackFaceCulling(triangles[i], normals[i], image);
         }
     }
 };
--- src/utils.hpp
+++ src/utils.hpp
@@ -16,11 +16,18 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 const TGAColor blue = TGAColor(0, 0, 255, 255);
 
+TGAColor randomColor()
+{
+    return TGAColor(rand() % 255, rand() % 255, rand() % 255, 255);
+}
+
 struct Point
 {
     float x;
     float y;
     float z;
+
+    TGAColor color;
 
     Point() : x(0), y(0), z(0) {}
     Point(float x, float y, float z) : x(x), y(y), z(z) {}
@@ -35,18 +42,73 @@ struct Point
 
 struct Triangle
 {
-    int p1;
-    int p2;
-    int p3;
+    Point p1;
+    Point p2;
+    Point p3;
+
+    float alpha;
+    float beta;
+    float gamma;
 
     Triangle() : p1(), p2(), p3() {}
-    Triangle(int p1, int p2, int p3) : p1(p1), p2(p2), p3(p3) {}
+    Triangle(Point p1, Point p2, Point p3) : p1(p1), p2(p2), p3(p3) {}
 
     // Overload the << operator to print the triangle
     friend std::ostream &operator<<(std::ostream &os, const Triangle &t)
     {
         os << "Triangle: {" << t.p1 << "; " << t.p2 << "; " << t.p3 << "}";
         return os;
+    }
+
+    void setBarycentricCoordinates(float a, float b, float c)
+    {
+        alpha = a;
+        beta = b;
+        gamma = c;
+    }
+};
+
+struct Vec3f
+{
+    float x, y, z;
+    Vec3f() : x(0), y(0), z(0) {}
+
+    Vec3f(float x, float y, float z) : x(x), y(y), z(z) {}
+
+    Vec3f operator-(const Vec3f &v) const
+    {
+        return Vec3f(x - v.x, y - v.y, z - v.z);
+    }
+
+    Vec3f operator^(const Vec3f &v) const
+    {
+        return Vec3f(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+    }
+
+    Vec3f normalize()
+    {
+        float norm = std::sqrt(x * x + y * y + z * z);
+        return Vec3f(x / norm, y / norm, z / norm);
+    }
+
+    float operator*(const Vec3f &v) const
+    {
+        return x * v.x + y * v.y + z * v.z;
+    }
+
+    Vec3f operator*(float k) const
+    {
+        return Vec3f(x * k, y * k, z * k);
+    }
+
+    Vec3f operator+(const Vec3f &v) const
+    {
+        return Vec3f(x + v.x, y + v.y, z + v.z);
+    }
+
+    Vec3f operator/(float k) const
+    {
+        return Vec3f(x / k, y / k, z / k);
     }
 };
 
@@ -85,9 +147,143 @@ void draw_line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
     }
 }
 
-void draw_line(Point p1, Point p2, TGAImage &image, TGAColor color)
+void draw_line(Point &p1, Point &p2, TGAImage &image, TGAColor color)
 {
     draw_line(p1.x, p1.y, p2.x, p2.y, image, color);
+}
+
+bool isInsideTriangle(Point &p, Triangle &t)
+{
+    float alpha = ((t.p2.y - t.p3.y) * (p.x - t.p3.x) + (t.p3.x - t.p2.x) * (p.y - t.p3.y)) /
+                  ((t.p2.y - t.p3.y) * (t.p1.x - t.p3.x) + (t.p3.x - t.p2.x) * (t.p1.y - t.p3.y));
+
+    float beta = ((t.p3.y - t.p1.y) * (p.x - t.p3.x) + (t.p1.x - t.p3.x) * (p.y - t.p3.y)) /
+                 ((t.p2.y - t.p3.y) * (t.p1.x - t.p3.x) + (t.p3.x - t.p2.x) * (t.p1.y - t.p3.y));
+
+    float gamma = 1.0f - alpha - beta;
+
+    t.setBarycentricCoordinates(alpha, beta, gamma);
+
+    return alpha > 0 && beta > 0 && gamma > 0;
+}
+
+void fillTriangle(Triangle &t, TGAImage &image, TGAColor color)
+{
+    // Bounding box
+    int minX = std::min(std::min(t.p1.x, t.p2.x), t.p3.x);
+    int minY = std::min(std::min(t.p1.y, t.p2.y), t.p3.y);
+    int maxX = std::max(std::max(t.p1.x, t.p2.x), t.p3.x);
+    int maxY = std::max(std::max(t.p1.y, t.p2.y), t.p3.y);
+
+    for (int x = minX; x <= maxX; x++)
+    {
+        for (int y = minY; y <= maxY; y++)
+        {
+            Point p = {(float)x, (float)y, 0.0};
+            if (isInsideTriangle(p, t))
+            {
+                image.set(x, y, color);
+            }
+        }
+    }
+}
+
+void fillTriangleLerp(Triangle &t, TGAImage &image)
+{
+    // Bounding box
+    int minX = std::min(std::min(t.p1.x, t.p2.x), t.p3.x);
+    int minY = std::min(std::min(t.p1.y, t.p2.y), t.p3.y);
+    int maxX = std::max(std::max(t.p1.x, t.p2.x), t.p3.x);
+    int maxY = std::max(std::max(t.p1.y, t.p2.y), t.p3.y);
+
+    for (int x = minX; x <= maxX; x++)
+    {
+        for (int y = minY; y <= maxY; y++)
+        {
+            Point p = {(float)x, (float)y, 0.0};
+            if (isInsideTriangle(p, t))
+            {
+                float alpha = t.alpha;
+                float beta = t.beta;
+                float gamma = t.gamma;
+
+                TGAColor color = TGAColor(
+                    alpha * t.p1.color.r + beta * t.p2.color.r + gamma * t.p3.color.r,
+                    alpha * t.p1.color.g + beta * t.p2.color.g + gamma * t.p3.color.g,
+                    alpha * t.p1.color.b + beta * t.p2.color.b + gamma * t.p3.color.b,
+                    255);
+
+                image.set(x, y, color);
+            }
+        }
+    }
+}
+
+void fillTriangleWithBackFaceCulling(Triangle &t, Triangle &normal, TGAImage &image)
+{
+    // Bounding box
+    int minX = std::min(std::min(t.p1.x, t.p2.x), t.p3.x);
+    int minY = std::min(std::min(t.p1.y, t.p2.y), t.p3.y);
+    int maxX = std::max(std::max(t.p1.x, t.p2.x), t.p3.x);
+    int maxY = std::max(std::max(t.p1.y, t.p2.y), t.p3.y);
+
+    // FIXME: This is not working
+    // if (t.p1.y > t.p2.y)
+    //     std::swap(t.p1, t.p2);
+    // if (t.p1.y > t.p3.y)
+    //     std::swap(t.p1, t.p3);
+    // if (t.p2.y > t.p3.y)
+    //     std::swap(t.p2, t.p3);
+
+    for (int x = minX; x <= maxX; x++)
+    {
+        for (int y = minY; y <= maxY; y++)
+        {
+            Point p = {(float)x, (float)y, 0.0};
+            if (isInsideTriangle(p, t))
+            {
+                Vec3f p1 = {t.p1.x, t.p1.y, t.p1.z};
+                Vec3f p2 = {t.p2.x, t.p2.y, t.p2.z};
+                Vec3f p3 = {t.p3.x, t.p3.y, t.p3.z};
+                Vec3f normal = (p3 - p1) ^ (p2 - p1);
+                Vec3f lightDir = Vec3f(0, 0, -1);
+                float dot = normal.normalize() * lightDir.normalize();
+                // std::clog << "dot: " << dot << "\n";
+                if (dot < 0)
+                {
+                    continue;
+                }
+
+                TGAColor color = TGAColor(255, 255, 255, 255);
+
+                float alpha = t.alpha;
+                float beta = t.beta;
+                float gamma = t.gamma;
+
+                color = TGAColor(
+                    alpha * t.p1.color.r + beta * t.p2.color.r + gamma * t.p3.color.r,
+                    alpha * t.p1.color.g + beta * t.p2.color.g + gamma * t.p3.color.g,
+                    alpha * t.p1.color.b + beta * t.p2.color.b + gamma * t.p3.color.b,
+                    255);
+
+                color.r = std::min(255, std::max(0, (int)(color.r * dot)));
+                color.g = std::min(255, std::max(0, (int)(color.g * dot)));
+                color.b = std::min(255, std::max(0, (int)(color.b * dot)));
+
+                image.set(x, y, color);
+            }
+        }
+    }
+}
+
+Point worldToScreen(Point p, int width, int height)
+{
+    // Denormalize the point to match the screen size
+    p.x = ((p.x + 1.) * 0.5 * width);
+    p.y = ((p.y + 1.) * 0.5 * height);
+    p.z *= 800;
+
+    return p;
 }
 
 #endif // UTILS_HPP
