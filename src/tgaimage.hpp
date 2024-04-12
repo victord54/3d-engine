0c2c579f6e19e9086dd4c5646f56def7c0929ee2
Victor Dallé <dallevictor@gmail.com>
2024-04-12 09:53:51 +0200

    format fichier

--- src/tgaimage.hpp
+++ src/tgaimage.hpp
@@ -33,21 +33,13 @@ struct TGAColor
     };
     int bytespp;
 
-    TGAColor() : val(0), bytespp(1)
-    {
-    }
+    TGAColor() : val(0), bytespp(1) {}
 
-    TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A) : b(B), g(G), r(R), a(A), bytespp(4)
-    {
-    }
+    TGAColor(unsigned char R, unsigned char G, unsigned char B, unsigned char A) : b(B), g(G), r(R), a(A), bytespp(4) {}
 
-    TGAColor(int v, int bpp) : val(v), bytespp(bpp)
-    {
-    }
+    TGAColor(int v, int bpp) : val(v), bytespp(bpp) {}
 
-    TGAColor(const TGAColor &c) : val(c.val), bytespp(c.bytespp)
-    {
-    }
+    TGAColor(const TGAColor &c) : val(c.val), bytespp(c.bytespp) {}
 
     TGAColor(const unsigned char *p, int bpp) : val(0), bytespp(bpp)
     {
